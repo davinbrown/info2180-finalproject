0ff9d5b5a5684b03ee9cca56f444f39795556864
davinbrown <davinbrown6@gmail.com>
2021-12-03 12:05:48 -0500

    updated xss page

--- xss.php
+++ xss.php
@@ -11,44 +11,41 @@
 	<head>
         <meta charset="utf-8">
 		<title>BugMe Issue Tracker</title>
-		<link href="xss.css" type="text/css" rel="stylesheet" />
+        <link href="./assets/css/styles.css" type="text/css" rel="stylesheet" />
 		<script src="./assets/js/issues.js"></script>
 	</head>
 
 	<body>
-    <h4>"header">BugMe Issue Tracker</h4>
+        <header>
+            <h4>BugMe Issue Tracker</h4>
+        </header>
         <div class="container">
             <?php include(ROOT_PATH . "/app/includes/nav.php"); ?>
             <main>
-                <?php $query = mysql_query("select *from issues", $connection);
-                while ($row = mysql_fetch_array($query)){
-                ?>
                 <div class = "issues1">
-                    <h2> <?php echo $rowl['title'];?> </h2>
-                    <p1> <?php echo $rowl['id'];?> </p1>
-                    <p> <?php echo $rowl['description'];?> </p> 
+                    <h2> title </h2>
+                    <p> id </p>
+                    <p>description </p> 
                     <br></br>
-                    <l1> issue created on <?php echo $rowl['created'];?> by <?php echo $rowl['created_by'];?>  </l1>
-                    <l1> Last updated on <?php echo $rowl['updated'];?> </l1>
+                    <l1> issue created on  by   </l1>
+                    <l1> Last updated on </l1>
+                </div>
+
+                <div class="flex-container">
+                    <h1> Assigned To</h1>
+                    <p> assign_to </p>
+                    <h1> Type: </h1>
+                    <p> type </p>
+                    <h1> Priority </h1>
+                    <p> priority </p> 
+                    <h1> Status:</h1>
+                    <p> status </p> 
+                    <br></br>
+                    <button id="Mark-as-closed-btn">Mar as Closed</button>
+                    <button id="Mark-inprogress-btn">Mark In Progress</button>
+                </div>
             </main>
 
         </div>
-
-        <div class="flex-container">
-            <R1> Assigned To</R1>
-            <p> <?php echo $rowl['assign_to'];?> </p>
-            <R1> Type: </R1>
-            <p> <?php echo $rowl['type'];?> </p>
-            <R1> Priority </R1>
-            <p> <?php echo $rowl['priority'];?> </p> 
-            <R1> Status:</R1>
-            <p> <?php echo $rowl['status'];?> </p> 
-            <br></br>
-            <button id="Mark-as-closed-btn">Mar as Closed</button>
-            <button2 id="Mark-inprogress-btn">Mark In Progress</button2>
-        </div>
-        
-
-
     </body>
 </html>
